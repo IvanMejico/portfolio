d98354b13816ebb7dccac21feb27c49b9b034766
ivanmejico <ivan.mejico21@gmail.com>
2020-10-07 15:54:41 +0800

    Updates

---
+++ precache-manifest.88cde2d3a6a8cd09b5676f2361f12bbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a937084175ef5b86d20bbf1f27a461",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7426b5cd78dc30ae0638",
+    "url": "/portfolio/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "9f54b9426a126577d5f1",
+    "url": "/portfolio/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "7426b5cd78dc30ae0638",
+    "url": "/portfolio/static/js/2.11047eb6.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/portfolio/static/js/2.11047eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f54b9426a126577d5f1",
+    "url": "/portfolio/static/js/main.4174f233.chunk.js"
+  },
+  {
+    "revision": "c3fe483896071cc46d75",
+    "url": "/portfolio/static/js/runtime-main.75c69042.js"
+  },
+  {
+    "revision": "54996365e891385736cdd875ca702a05",
+    "url": "/portfolio/static/media/resume.54996365.pdf"
+  }
+]);
